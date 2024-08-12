57f1a4ceda989dd0a060fcd823a57b5d867436af
o.name <daviolie2@gmail.com>
2024-08-12 14:59:30 -0300

    Making validation on category

--- obj/Debug/net8.0/IWantApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/IWantApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e621a7c630b599da0865972afd65d7eb0bb6e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b66025e7e2e65183f73773e19c79a6dbe6deec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IWantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
